bf93e61397091f9e5a93442735530b12f448fe90
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-09-12 09:44:38 -0400

    Done

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Create IB outlets
+    @IBOutlet weak var topLeft: UILabel!
+    @IBOutlet weak var bottomRight: UILabel!
+    @IBOutlet weak var center: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
-    // TODO: IB actions and code to update UI
+    
+    @IBAction func club(sender: AnyObject) {
+        center.text = "4"
+        topLeft.text = "♣️"
+        bottomRight.text = "♣️"
+    }
+    
+    
+    @IBAction func spade(sender: AnyObject) {
+        center.text = "3"
+        topLeft.text = "♠️"
+        bottomRight.text = "♠️"
+    }
+    
+    @IBAction func diamond(sender: AnyObject) {
+        center.text = "8"
+        topLeft.text = "♦️"
+        bottomRight.text = "♦️"
+    }
+    
+    @IBAction func heart(sender: AnyObject) {
+        center.text = "10"
+        topLeft.text = "♥️"
+        bottomRight.text = "♥️"
+    }
+    
+    
 }
